8c52b0386cce1174c5071aa9a8e99c165aa0c825
Cristina Aldea <kris18a@yahoo.com>
2016-06-02 09:17:33 -0400

    More changes

--- GoogleMapsApiWS/src/main/java/com/google/maps/ws/client/WSClient.java
+++ GoogleMapsApiWS/src/main/java/com/google/maps/ws/client/WSClient.java
@@ -13,46 +13,46 @@ import java.net.URL;
  * Created by X on 31/05/2016.
  */
 public class WSClient {
-    public static void main(String[] args) {
-        try {
-            String urlString = "http://localhost:8080/searchPlaces?location=43.768353,-79.41304630000002&type=restaurant";
-            if (args != null && args.length > 0) {
-                urlString = args[0];
-            }
-            URL url = new URL(urlString);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-            conn.setRequestProperty("Accept", "application/json");
-
-            if (conn.getResponseCode() != 200) {
-                throw new RuntimeException("Failed : HTTP error code : "
-                        + conn.getResponseCode());
-            }
-
-            BufferedReader br = new BufferedReader(new InputStreamReader(
-                    (conn.getInputStream())));
-
-            String output;
-            System.out.println("Output from Server .... \n");
-            StringBuilder sb = new StringBuilder();
-            while ((output = br.readLine()) != null) {
-                sb.append(output);
-                sb.append(System.getProperty("line.separator"));
-            }
-
-            conn.disconnect();
-
-            JSONObject json = new JSONObject(sb.toString()); // Convert text to object
-            System.out.println(json.toString(4)); // Print it with specified indentation
-
-        } catch (MalformedURLException e) {
-
-            e.printStackTrace();
-
-        } catch (IOException e) {
-
-            e.printStackTrace();
-
-        }
-    }
+//    public static void main(String[] args) {
+//        try {
+//            String urlString = "http://localhost:8080/searchPlaces?location=43.768353,-79.41304630000002&type=restaurant";
+//            if (args != null && args.length > 0) {
+//                urlString = args[0];
+//            }
+//            URL url = new URL(urlString);
+//            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+//            conn.setRequestMethod("GET");
+//            conn.setRequestProperty("Accept", "application/json");
+//
+//            if (conn.getResponseCode() != 200) {
+//                throw new RuntimeException("Failed : HTTP error code : "
+//                        + conn.getResponseCode());
+//            }
+//
+//            BufferedReader br = new BufferedReader(new InputStreamReader(
+//                    (conn.getInputStream())));
+//
+//            String output;
+//            System.out.println("Output from Server .... \n");
+//            StringBuilder sb = new StringBuilder();
+//            while ((output = br.readLine()) != null) {
+//                sb.append(output);
+//                sb.append(System.getProperty("line.separator"));
+//            }
+//
+//            conn.disconnect();
+//
+//            JSONObject json = new JSONObject(sb.toString()); // Convert text to object
+//            System.out.println(json.toString(4)); // Print it with specified indentation
+//
+//        } catch (MalformedURLException e) {
+//
+//            e.printStackTrace();
+//
+//        } catch (IOException e) {
+//
+//            e.printStackTrace();
+//
+//        }
+//    }
 }
